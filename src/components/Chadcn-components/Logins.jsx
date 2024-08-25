e3368ad396ea3a80066ae6f748614d8fa5d374df
RonnyToledo <ronnytoledo87@gmail.com>
2024-08-25 13:55:25 -0400

    Primer commit

--- src/components/Chadcn-components/Logins.jsx
+++ src/components/Chadcn-components/Logins.jsx
@@ -64,8 +64,6 @@ export default function Logins({ ThemeContext }) {
     fetchData(webshop.store.sitioweb);
   }, [webshop]);
 
-  console.log(logins);
-
   return (
     <div className="chart-wrapper mx-auto flex max-w-6xl flex-col flex-wrap items-start justify-center gap-6 p-6 sm:flex-row sm:p-8">
       <div className="grid w-full gap-6 sm:grid-cols-2 lg:max-w-[22rem] lg:grid-cols-1 xl:max-w-[25rem]">
